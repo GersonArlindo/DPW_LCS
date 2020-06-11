5adef518d939f734f1b02345a015b800efe01a24
Gerson Arlindo <gerson50039@gmail.com>
2020-06-11 16:31:26 -0600

    primer commit

---
+++ js/alert.js
@@ -0,0 +1,14 @@
+function cargarInicio(){
+Swal.fire({
+  title: 'LCS Web 2020.',
+  width: 470,
+  padding: '3em',
+  background: '#fff url(https://image.shutterstock.com/image-vector/abstract-elegant-background-design-space-260nw-1537970075.jpg)',
+  backdrop: `
+    rgba(0,0,123,0.4)
+    left top
+    no-repeat
+  `
+});
+}
+ cargarInicio();
